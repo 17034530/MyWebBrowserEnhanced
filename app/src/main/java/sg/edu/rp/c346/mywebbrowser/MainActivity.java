45bbc47331675bad6869a6f69d34474af1b34130
17034530 <17034530@myrp.edu.sg>
2018-08-08 12:14:53 +0800

    n

--- app/src/main/java/sg/edu/rp/c346/mywebbrowser/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/mywebbrowser/MainActivity.java
@@ -7,10 +7,13 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLoadURL;
     WebView wvMyPage;
+    EditText URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +23,23 @@ public class MainActivity extends AppCompatActivity {
         btnLoadURL = findViewById(R.id.buttonLoad);
         wvMyPage = findViewById(R.id.webViewMyPage);
         wvMyPage.setWebViewClient(new WebViewClient());
+        URL = findViewById(R.id.editTextURL);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+        wvMyPage.getSettings().setJavaScriptEnabled(true);
+        wvMyPage.getSettings().setAllowFileAccess(false);
+        wvMyPage.getSettings().setBuiltInZoomControls(true);
 
         btnLoadURL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = "https://www.rp.edu.sg";
-                wvMyPage.loadUrl(url);
-                WebSettings webSettings = wvMyPage.getSettings();
-                webSettings.setJavaScriptEnabled(true);
-                webSettings.setAllowFileAccess(false);
-                webSettings.setBuiltInZoomControls(true);
+                if(URL.getText().toString().length()!=0) {
+                    String url = URL.getText().toString();
+                    wvMyPage.loadUrl(url);
+                }else{
+                    Toast.makeText(MainActivity.this,"write something",Toast.LENGTH_LONG).show();
+                    return;
+                }
             }
         });
     }
